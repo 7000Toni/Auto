2cd143f7caa17da3ea94b8f3d3bf7c85ad5937f6
7000Toni <7000toni@gmail.com>
2026-06-01 03:20:37 +0200

    redraw charts after chart hsb is moved

--- src/com/github/_7000toni/auto/canvasnode/scrollbar/HorizontalChartScrollBar.java
+++ src/com/github/_7000toni/auto/canvasnode/scrollbar/HorizontalChartScrollBar.java
@@ -82,6 +82,7 @@ public class HorizontalChartScrollBar extends HorizontalScrollBar {
 			((Chart) sbo).setKeepStartIndex(false);
 			CrossHair.setX(e.getX());
 			CrossHair.setY(e.getY());
+			Chart.drawCharts(((Chart) sbo).name());
 		}
 	}
 	
